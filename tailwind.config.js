68ded4c31ae3ff9bbc1fe00c28858733218c6f48
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-09 11:50:48 +0800

    feat: add default colors scheme (#4)

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        'honey': '#FFCE00',
+        'modern-black': '#1A1A1A',
+        'modern-white': '#F7F7F7'
+      },
       fontFamily: {
         poppins: ['Poppins', ...defaultTheme.fontFamily.mono]
       }
