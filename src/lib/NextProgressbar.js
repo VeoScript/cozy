c32c09661b3cdb51ebcf69400f8a1e5e370ade55
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-09 19:15:11 +0800

    style: change progressbar color (#7)

--- src/lib/NextProgressbar.js
+++ src/lib/NextProgressbar.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 /* eslint-disable react/prefer-stateless-function */
 class NextNProgress extends React.Component {
   static defaultProps = {
-    color: '#FD823E',
+    color: '#FFFCF0',
     startPosition: 0.3,
     stopDelayMs: 200,
     height: 3,
