724875767cd3b0ae185bcc1a64b993180eedaea5
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-09 22:47:05 +0800

    chore: change cookie name

--- src/lib/Session.js
+++ src/lib/Session.js
@@ -4,7 +4,7 @@ import { withIronSession } from 'next-iron-session'
 export default function withSession(handler) {
   return withIronSession(handler, {
     password: "complex_password_at_least_32_characters_long",
-    cookieName: "myapp_cookiename",
+    cookieName: "cozy_cookie",
     // if your localhost is served on http:// then disable the secure flag
     cookieOptions: {
       secure: process.env.NODE_ENV === "production",
