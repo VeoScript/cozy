6ad322acac4d986fbf5bdf2ff741b1a3052aff3b
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-09 22:45:44 +0800

    feat: add session wrapper (#10)

---
+++ src/lib/Session.js
@@ -0,0 +1,13 @@
+// this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
+import { withIronSession } from 'next-iron-session'
+
+export default function withSession(handler) {
+  return withIronSession(handler, {
+    password: "complex_password_at_least_32_characters_long",
+    cookieName: "myapp_cookiename",
+    // if your localhost is served on http:// then disable the secure flag
+    cookieOptions: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  })
+}
