a23dab24c135ddecbf4b1de90315449034bb2f84
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-09 11:37:54 +0800

    feat: add tailwindcss (#2)

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div>
+        <h1>Cozy Homepage</h1>
+      </div>
+    </>
+  )
+}
