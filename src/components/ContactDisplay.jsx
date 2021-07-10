61c751878f19f2f2921e668f75f85a9a68b1d512
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-10 20:19:15 +0800

    style: update contact-display padding (#26)

--- src/components/ContactDisplay.jsx
+++ src/components/ContactDisplay.jsx
@@ -1,12 +1,12 @@
 export default function ContactDisplay() {
   return (
     <div className="font-poppins hidden md:flex flex-col w-full">
-      <div className="flex flex-row items-center justify-between w-full px-10 py-8 border-b border-modern-white border-opacity-10">
-        <div className="searchinput flex flex-row items-center w-full max-w-[16rem] border-b border-gray-500 text-gray-500 py-3 space-x-2">
-          <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <div className="flex flex-row items-center justify-between w-full px-10 py-5 border-b border-modern-white border-opacity-10">
+        <div className="searchinput flex flex-row items-center w-full max-w-xs border-b border-gray-500 text-gray-500 py-3 space-x-2">
+          <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
           </svg>
-          <input className="w-full bg-modern-black font-light text-sm text-gray-300 focus:outline-none" type="text" name="search" placeholder="Search" />
+          <input className="w-full bg-modern-black font-light text-xs text-gray-300 focus:outline-none" type="text" name="search" placeholder="Search" />
         </div>
         <button className="flex items-center justify-center w-full max-w-[8rem] px-2 py-3 text-xs rounded-lg transition ease-in-out duration-200 transform hover:scale-95 space-x-1 bg-honey text-modern-black">
           <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -16,13 +16,13 @@ export default function ContactDisplay() {
         </button>
       </div>
       <div className="contact-list flex flex-col items-center w-full h-full overflow-y-auto">
-        <div className="flex flex-row items-center justify-between w-full text-gray-400 px-5 py-5 space-x-10">
+        <div className="flex flex-row items-center justify-between w-full text-gray-400 px-5 py-3 space-x-10">
           <span className="w-full max-w-[40px] text-xs">Profile</span>
           <span className="w-full max-w-sm text-xs">Name</span>
           <span className="w-full max-w-xs text-xs">Phone</span>
           <span className="w-full max-w-xs text-xs">Actions</span>
         </div>
-        <div className="flex flex-row items-center justify-between w-full px-5 py-5 space-x-10 bg-modern-dim">
+        <div className="flex flex-row items-center justify-between w-full px-5 py-5 space-x-10 border-t border-b border-modern-white border-opacity-10 bg-modern-dim">
           <img src="https://64.media.tumblr.com/777b82fbdc6768d1b2e5f02957ff9e51/acfd59a2153ef06e-4f/s1280x1920/46e004f9c43a907c532da035b6831f3797373b4d.jpg" className="w-full max-w-[40px] h-10 object-cover rounded-full" />
           <span className="w-full max-w-sm text-xs">Jennie Kim</span>
           <span className="w-full max-w-sm text-xs">09759863533</span>
