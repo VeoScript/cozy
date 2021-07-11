98de14f6fcf149f939ae8cd059450ff31f9d6984
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-11 18:53:04 +0800

    feat: add dummy contacts

---
+++ src/static/faker.js
@@ -0,0 +1,46 @@
+export const people = [
+  {
+    'avatar': 'https://i.pinimg.com/originals/00/5b/6d/005b6d6b9c52da58c32baf2ac351b331.jpg',
+    'name': 'Lisa Manoban',
+    'phone': '09067545445',
+    'email': 'lisamanoban@gmail.com',
+    'facebook': '/',
+    'twitter': '/',
+    'instagram': '/',
+    'tiktok': '/',
+    'youtube': ''
+  },
+  {
+    'avatar': 'https://cf.shopee.ph/file/46f7ebaa7dbac43476f3c328935d79c0',
+    'name': 'Jennie Kim',
+    'phone': '09568824553',
+    'email': 'jenniekim@gmail.com',
+    'facebook': '/',
+    'twitter': '/',
+    'instagram': '/',
+    'tiktok': '/',
+    'youtube': ''
+  },
+  {
+    'avatar': 'https://lh3.googleusercontent.com/VTX8O3fEDZcfpFsEwQKM_E1sEjTVw9HExPEybwvUr81XzcM2nYz5PDPqAVQuG-DDcaiqJZUK8SgePosw2_8ecB5zLniyyz-uBT-ndrWLsrubvQO5=s0',
+    'name': 'Roseanne Park',
+    'phone': '09456784334',
+    'email': 'roseannepark@gmail.com',
+    'facebook': '/',
+    'twitter': '/',
+    'instagram': '/',
+    'tiktok': '/',
+    'youtube': ''
+  },
+  {
+    'avatar': 'https://pbs.twimg.com/media/EzH3-JTXIAAH6H9.jpg',
+    'name': 'Kim Jisoo',
+    'phone': '09854352626',
+    'email': 'kimjisoo@gmail.com',
+    'facebook': '/',
+    'twitter': '/',
+    'instagram': '/',
+    'tiktok': '/',
+    'youtube': ''
+  }
+]
