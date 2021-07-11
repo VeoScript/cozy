abd69b4f90ce6da89970a69e8a7e2a66d8d19b81
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-11 18:53:26 +0800

    feat: add dummy contacts (#28)

--- src/components/ContactDisplay.jsx
+++ src/components/ContactDisplay.jsx
@@ -1,7 +1,9 @@
+import { people } from '~/static/faker'
+
 export default function ContactDisplay() {
   return (
     <div className="font-poppins hidden md:flex flex-col w-full">
-      <div className="flex flex-row items-center justify-between w-full px-10 py-5 border-b border-modern-white border-opacity-10">
+      <div className="flex flex-row items-center justify-between w-full px-5 py-5 border-b border-modern-white border-opacity-10">
         <div className="searchinput flex flex-row items-center w-full max-w-xs border-b border-gray-500 text-gray-500 py-3 space-x-2">
           <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
@@ -18,32 +20,34 @@ export default function ContactDisplay() {
       <div className="contact-list flex flex-col items-center w-full h-full overflow-y-auto">
         <div className="flex flex-row items-center justify-between w-full text-gray-400 px-5 py-3 space-x-10">
           <span className="w-full max-w-[40px] text-xs">Profile</span>
-          <span className="w-full max-w-[14rem] text-xs">Name</span>
-          <span className="w-full max-w-[28rem] text-xs">Phone</span>
+          <span className="w-full max-w-[14rem] text-xs pl-3">Name</span>
+          <span className="w-full max-w-[28rem] text-xs pl-5">Phone</span>
           <span className="w-full max-w-[8rem] text-xs">Actions</span>
         </div>
-        <div className="flex flex-row items-center justify-between w-full px-5 py-5 space-x-10 border-t border-b border-modern-white border-opacity-10 bg-modern-dim">
-          <img src="https://64.media.tumblr.com/777b82fbdc6768d1b2e5f02957ff9e51/acfd59a2153ef06e-4f/s1280x1920/46e004f9c43a907c532da035b6831f3797373b4d.jpg" className="w-full max-w-[40px] h-10 object-cover rounded-full" />
-          <span className="w-full max-w-sm text-xs">Jennie Kim</span>
-          <span className="w-full max-w-sm text-xs">09759863533</span>
-          <span className="flex items-center justify-end w-full max-w-sm text-xs space-x-1">
-            <button className="transition ease-in-out duration-200 bg-modern-black px-3 py-3 rounded-full hover:scale-95">
-              <svg className="w-4 h-4 text-modern-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z"></path>
-              </svg>
-            </button>
-            <button className="transition ease-in-out duration-200 bg-modern-black px-3 py-3 rounded-full hover:scale-95">
-              <svg className="w-4 h-4 text-honey" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
-              </svg>
-            </button>
-            <button className="transition ease-in-out duration-200 bg-modern-black px-3 py-3 rounded-full hover:scale-95">
-              <svg className="w-4 h-4 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
-              </svg>
-            </button>
-          </span>
-        </div>
+        {people.map(({ avatar, name, phone }, i) => (
+          <div className="flex flex-row items-center justify-between w-full px-5 py-5 space-x-10 border-t border-b border-modern-white border-opacity-10 bg-modern-dim" key={i}>
+            <img src={avatar} className="w-full max-w-[56px] h-14 object-cover rounded-full" />
+            <span className="w-full max-w-sm text-sm">{name}</span>
+            <span className="w-full max-w-sm text-sm">{phone}</span>
+            <span className="flex items-center justify-end w-full max-w-sm text-xs space-x-1">
+              <button className="transition ease-in-out duration-200 bg-modern-black px-3 py-3 rounded-full hover:scale-95">
+                <svg className="w-4 h-4 text-modern-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z"></path>
+                </svg>
+              </button>
+              <button className="transition ease-in-out duration-200 bg-modern-black px-3 py-3 rounded-full hover:scale-95">
+                <svg className="w-4 h-4 text-honey" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
+                </svg>
+              </button>
+              <button className="transition ease-in-out duration-200 bg-modern-black px-3 py-3 rounded-full hover:scale-95">
+                <svg className="w-4 h-4 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+                </svg>
+              </button>
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   )
--- src/components/FavoriteDisplay.jsx
+++ src/components/FavoriteDisplay.jsx
@@ -4,6 +4,7 @@ import InstagramSmall from '~/lib/icons/socialmedia/InstagramSmall'
 import TwitterSmall from '~/lib/icons/socialmedia/TwitterSmall'
 import TikTokSmall from '~/lib/icons/socialmedia/TikTokSmall'
 import YouTubeSmall from '~/lib/icons/socialmedia/YouTubeSmall'
+import { people } from '~/static/faker'
 
 export default function FavoriteDisplay() {
   return (
@@ -20,52 +21,54 @@ export default function FavoriteDisplay() {
         </div>
       </div>
       <div className="grid grid-rows md:grid-cols-3 grid-flow-row gap-x-4 gap-y-16 w-full max-w-fll h-full overflow-y-auto py-16 px-5 mb-3">
-        <div className="flex flex-col w-full max-w-full md:max-w-md h-full max-h-[13rem] rounded-3xl px-5 py-5 space-y-5 bg-modern-dim">
-          <div className="flex flex-row justify-center items-center w-full">
-            <div className="flex justify-center w-full -mt-16 ml-10">
-              <img className="w-24 h-24 object-cover rounded-full" src="https://64.media.tumblr.com/777b82fbdc6768d1b2e5f02957ff9e51/acfd59a2153ef06e-4f/s1280x1920/46e004f9c43a907c532da035b6831f3797373b4d.jpg" />
+        {people.map(({ avatar, name, phone, email, facebook, twitter, instagram, tiktok, youtube }, i) => (
+          <div className="flex flex-col w-full max-w-full md:max-w-md h-full max-h-[13rem] rounded-3xl px-5 py-5 space-y-5 bg-modern-dim" key={i}>
+            <div className="flex flex-row justify-center items-center w-full">
+              <div className="flex justify-center w-full -mt-16 ml-10">
+                <img className="w-24 h-24 object-cover rounded-full" src={avatar} />
+              </div>
+              <div className="flex flex-row justify-end">
+                <button className="text-red-600 transition ease-in-out duration-300 hover:scale-95 focus:outline-none">
+                  <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd"></path>
+                  </svg>
+                </button>
+              </div>
             </div>
-            <div className="flex flex-row justify-end">
-              <button className="text-red-600 transition ease-in-out duration-300 hover:scale-95 focus:outline-none">
-                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd"></path>
-                </svg>
-              </button>
+            <div className="flex flex-col items-center w-full">
+              <span className="text-lg md:text-lg">{name}</span>
+              <span className="text-xs md:text-base text-gray-400">{phone}</span>
+              <span className="text-xs md:text-sm text-gray-400 mt-1">{email}</span>
+              <div className="flex flex-row items-center justify-center w-full mt-3 space-x-2">
+                <Link href={facebook}>
+                  <a target="_blank">
+                    <FacebookSmall />
+                  </a>
+                </Link>
+                <Link href={twitter}>
+                  <a target="_blank">
+                    <TwitterSmall />
+                  </a>
+                </Link>
+                <Link href={instagram}>
+                  <a target="_blank">
+                    <InstagramSmall />
+                  </a>
+                </Link>
+                <Link href={tiktok}>
+                  <a target="_blank">
+                    <TikTokSmall />
+                  </a>
+                </Link>
+                <Link href={youtube}>
+                  <a target="_blank">
+                    <YouTubeSmall />
+                  </a>
+                </Link>
+              </div>
             </div>
           </div>
-          <div className="flex flex-col items-center w-full">
-            <span className="text-lg">Jennie Kim</span>
-            <span className="text-xs text-gray-400">09759863533</span>
-            <span className="text-xs text-gray-400 mt-1">jenniekim@gmail.com</span>
-            <div className="flex flex-row items-center justify-center w-full mt-3 space-x-2">
-              <Link href="/">
-                <a target="_blank">
-                  <FacebookSmall />
-                </a>
-              </Link>
-              <Link href="/">
-                <a target="_blank">
-                  <TwitterSmall />
-                </a>
-              </Link>
-              <Link href="/">
-                <a target="_blank">
-                  <InstagramSmall />
-                </a>
-              </Link>
-              <Link href="/">
-                <a target="_blank">
-                  <TikTokSmall />
-                </a>
-              </Link>
-              <Link href="/">
-                <a target="_blank">
-                  <YouTubeSmall />
-                </a>
-              </Link>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   )
