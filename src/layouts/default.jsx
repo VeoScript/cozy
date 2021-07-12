2e29d795279400db49b5aed11da5850f0798faf6
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-12 09:35:28 +0800

    refactor: update default layout (#31)

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -1,7 +1,12 @@
+import SideBar from '~/components/SideBar'
+
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col items-center justify-center w-full max-w-full h-screen overflow-hidden bg-modern-dim">
-      { children }
+      <div className="flex flex-row justify-center w-full max-w-screen-2xl h-full bg-honey">
+        <SideBar />
+        { children }
+      </div>
     </div>
   )
 }
