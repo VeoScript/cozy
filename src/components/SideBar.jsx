eced550295a93addff568f519054b7d5a27d7356
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-12 11:15:01 +0800

    style: remove bg-opacity on hover (#34)

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -15,7 +15,7 @@ export default function SideBar() {
         <div className="flex flex-col space-y-5">
           {navigations.map(({ icon, href }, i) => (
             <Link href={href} key={i}>
-              <a className={`${router.pathname === href ? 'bg-modern-dim text-honey' : 'bg-none text-[#333]'} px-3 py-3 rounded-full transition ease-in-out duration-300 hover:bg-modern-black hover:bg-opacity-80 hover:text-honey`}>{ icon }</a>
+              <a className={`${router.pathname === href ? 'bg-modern-dim text-honey' : 'bg-none text-[#333]'} px-3 py-3 rounded-full transition ease-in-out duration-300 hover:bg-modern-black hover:text-honey`}>{ icon }</a>
             </Link>
           ))}
         </div>
@@ -30,7 +30,7 @@ export default function SideBar() {
       <div className="fixed md:hidden bottom-0 z-50 flex flex-row items-center justify-between w-full px-5 bg-honey">
         {mobile_navigations.map(({ icon, href }, i) => (
           <Link href={href} key={i}>
-            <a className={`${router.pathname === href ? 'bg-modern-dim text-honey border-4 border-honey -translate-y-3' : 'bg-none text-[#333]'} px-3 py-3 rounded-full transition ease-in-out duration-300 hover:bg-modern-black hover:bg-opacity-80 hover:text-honey`}>{ icon }</a>
+            <a className={`${router.pathname === href ? 'bg-modern-dim text-honey border-4 border-honey -translate-y-3' : 'bg-none text-[#333]'} px-3 py-3 rounded-full transition ease-in-out duration-300 hover:bg-modern-black hover:text-honey`}>{ icon }</a>
           </Link>
         ))}
       </div>
