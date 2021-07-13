dcad000c36024a33d0f9476db6352171d05b18d3
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-13 13:41:58 +0800

    style: change max-width (#47)

--- src/components/Dialogs/Contacts/ViewContact.jsx
+++ src/components/Dialogs/Contacts/ViewContact.jsx
@@ -62,7 +62,7 @@ export default function ViewContact({ avatar, name, phone, email, address, faceb
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="inline-block w-full max-w-sm p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-modern-black text-modern-white shadow-xl rounded-2xl">
+              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-modern-black text-modern-white shadow-xl rounded-2xl">
                 <div className="relative">
                   <div className="fixed z-10 top-5 right-5">
                     <button
