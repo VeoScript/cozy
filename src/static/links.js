c32428b935c7e32ad35fbf37bbedf67dd57aea88
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-13 13:37:11 +0800

    chore: change navigations position

--- src/static/links.js
+++ src/static/links.js
@@ -5,14 +5,14 @@ export const navigations = [
     'href': '/'
   },
   {
-    'name': 'Favorites',
-    'icon': <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></path></svg>,
-    'href': '/favorites'
-  },
-  {
     'name': 'Diary',
     'icon': <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>,
     'href': '/diary'
+  },
+  {
+    'name': 'Favorites',
+    'icon': <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></path></svg>,
+    'href': '/favorites'
   },
   {
     'name': 'Messages',
