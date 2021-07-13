7355c2e71b9b745a42c0dac17a1a4ca2b7612d31
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-13 12:03:07 +0800

    feat: create dialogs for create, view, edit and delete contact (#44)
    chore: remove other props

--- src/components/ContactDisplay.jsx
+++ src/components/ContactDisplay.jsx
@@ -1,4 +1,7 @@
 import CreateNew from './Dialogs/Contacts/CreateNew'
+import ViewContact from './Dialogs/Contacts/ViewContact'
+import UpdateContact from './Dialogs/Contacts/UpdateContact'
+import DeleteContact from './Dialogs/Contacts/DeleteContact'
 import Scrollbar from 'react-smooth-scrollbar'
 import { people } from '~/static/faker'
 
@@ -16,14 +19,14 @@ export default function ContactDisplay() {
       </div>
       <Scrollbar>
         <div className="contact-list flex flex-col items-center w-full h-full overflow-y-auto overflow-x-hidden">
-          <div className="flex flex-row items-center justify-between w-full text-gray-400 px-5 py-3 space-x-10">
+          <div className="flex flex-row items-center justify-between w-full text-gray-400 px-5 py-3 space-x-10 border-b border-modern-white border-opacity-10">
             <span className="w-full max-w-[40px] text-xs">Profile</span>
             <span className="w-full max-w-[14rem] text-xs pl-3">Name</span>
             <span className="w-full max-w-[28rem] text-xs pl-5">Phone</span>
             <span className="w-full max-w-[10rem] text-xs">Actions</span>
           </div>
-          {people.map(({ avatar, name, phone }, i) => (
-            <div className="flex flex-row items-center justify-between w-full px-5 py-5 space-x-10 border-t border-b border-modern-white border-opacity-10 bg-modern-dim" key={i}>
+          {people.map(({ avatar, name, phone, email, address, facebook, instagram, twitter, tiktok, youtube }, i) => (
+            <div className="flex flex-row items-center justify-between w-full px-5 py-5 space-x-10 border-b border-modern-white border-opacity-10 bg-modern-dim" key={i}>
               <img src={avatar} className="w-full max-w-[56px] h-14 object-cover rounded-full" />
               <span className="w-full max-w-sm text-sm">{name}</span>
               <span className="w-full max-w-sm text-sm">{phone}</span>
@@ -33,22 +36,33 @@ export default function ContactDisplay() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
                   </svg>
                 </button>
-                <button className="transition ease-in-out duration-200 bg-modern-black text-gray-400 px-3 py-3 rounded-full hover:scale-95">
-                  <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                  </svg>
-                </button>
-                <button className="transition ease-in-out duration-200 bg-modern-black text-honey px-3 py-3 rounded-full hover:scale-95">
-                  <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
-                  </svg>
-                </button>
-                <button className="transition ease-in-out duration-200 bg-modern-black text-red-800 px-3 py-3 rounded-full hover:scale-95">
-                  <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
-                  </svg>
-                </button>
+                <ViewContact
+                  avatar={avatar}
+                  name={name}
+                  phone={phone}
+                  email={email}
+                  address={address}
+                  facebook={facebook}
+                  instagram={instagram}
+                  twitter={twitter}
+                  tiktok={tiktok}
+                  youtube={youtube}
+                />
+                <UpdateContact
+                  avatar={avatar}
+                  name={name}
+                  phone={phone}
+                  email={email}
+                  address={address}
+                  facebook={facebook}
+                  instagram={instagram}
+                  twitter={twitter}
+                  tiktok={tiktok}
+                  youtube={youtube}
+                />
+                <DeleteContact
+                  name={name}
+                />
               </span>
             </div>
           ))}
---
+++ src/components/Dialogs/Contacts/ViewContact.jsx
@@ -0,0 +1,166 @@
+import Link from 'next/link'
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+
+export default function ViewContact({ avatar, name, phone, email, address, facebook, instagram, twitter, tiktok, youtube }) {
+
+  let [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  return (
+    <>
+      <button
+        className="transition ease-in-out duration-200 bg-modern-black text-gray-400 px-3 py-3 rounded-full hover:scale-95 focus:outline-none"
+        type="button"
+        onClick={openModal}
+      >
+        <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+        </svg>
+      </button>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          onClose={closeModal}
+        >
+          <div className="min-h-screen px-4 pb-10 md:pb-0 text-center bg-modern-white bg-opacity-20">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-sm p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-modern-black text-modern-white shadow-xl rounded-2xl">
+                <div className="relative">
+                  <div className="fixed z-10 top-5 right-5">
+                    <button
+                        className="transition ease-in-out duration-200 hover:scale-90 focus:outline-none"
+                        type="button"
+                        onClick={closeModal}
+                      >
+                        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                          <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                        </svg>
+                      </button>
+                  </div>
+                </div>
+                <div className="my-5">
+                  <div className="flex flex-col items-center w-full space-y-5">
+                    <div className="flex">
+                      <img className="w-32 h-32 object-cover rounded-full bg-modern-dim" src={avatar} alt="profile" />
+                    </div>
+                    <div className="flex flex-col items-center w-full">
+                      <div className="flex flex-row items-center justify-center w-full mb-3 space-x-2">
+                        <Link href={facebook}>
+                          <a className={`${!facebook ? 'hidden' : 'block'} transition ease-in-out duration-300 hover:scale-95`} target="_blank">
+                            <FacebookIcon />
+                          </a>
+                        </Link>
+                        <Link href={twitter}>
+                          <a className={`${!twitter ? 'hidden' : 'block'}`} target="_blank">
+                            <TwitterIcon />
+                          </a>
+                        </Link>
+                        <Link href={instagram}>
+                          <a className={`${!instagram ? 'hidden' : 'block'} transition ease-in-out duration-300 hover:scale-95`} target="_blank">
+                            <InstagramIcon />
+                          </a>
+                        </Link>
+                        <Link href={tiktok}>
+                          <a className={`${!tiktok ? 'hidden' : 'block'} transition ease-in-out duration-300 hover:scale-95`} target="_blank">
+                            <TiktokIcon />
+                          </a>
+                        </Link>
+                        <Link href={youtube}>
+                          <a className={`${!youtube ? 'hidden' : 'block'} transition ease-in-out duration-300 hover:scale-95`} target="_blank">
+                            <YoutubeIcon />
+                          </a>
+                        </Link>
+                      </div>
+                      <span className="text-lg md:text-xl">{name}</span>
+                      <span className="text-xs md:text-base text-gray-400">{phone}</span>
+                      <span className="text-xs md:text-sm text-gray-400 mt-1">{email}</span>
+                      <span className="text-[10px] md:text-sm text-gray-400 mt-1">{address}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
+
+function FacebookIcon() {
+  return (
+    <svg className="w-5 h-5 fill-current text-modern-white opacity-80" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/>
+    </svg>
+  )
+}
+
+function InstagramIcon() {
+  return (
+    <svg className="w-5 h-5 fill-current text-modern-white opacity-80" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+    </svg>
+  )
+}
+
+function TwitterIcon() {
+  return (
+    <svg className="w-5 h-5 fill-current text-modern-white opacity-80" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/>
+    </svg>
+  )
+}
+
+function TiktokIcon() {
+  return (
+    <svg className="w-5 h-5 fill-current text-modern-white opacity-80" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z"/>
+    </svg>
+  )
+}
+
+function YoutubeIcon() {
+  return (
+    <svg className="w-5 h-5 fill-current text-modern-white opacity-80" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/>
+    </svg>
+  )
+}
