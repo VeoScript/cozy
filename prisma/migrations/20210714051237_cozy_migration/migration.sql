bc7d4c17d27996053ff92a0fbdd1456d89715487
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-14 13:14:10 +0800

    feat: update the migration and schema (#59)

--- prisma/migrations/20210714051237_cozy_migration/migration.sql
+++ prisma/migrations/20210714051237_cozy_migration/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "avatar" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
 
@@ -46,14 +47,30 @@ CREATE TABLE "Favorites" (
 );
 
 -- CreateTable
-CREATE TABLE "Messages" (
+CREATE TABLE "Diary" (
     "id" SERIAL NOT NULL,
-    "message" TEXT,
-    "sender" TEXT,
+    "photo" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "date" TEXT NOT NULL,
     "userId" INTEGER NOT NULL,
 
     PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "Messages" (
+    "id" SERIAL NOT NULL,
+    "message" TEXT,
+    "sender" TEXT,
+    "date" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User.username_unique" ON "User"("username");
@@ -70,11 +87,17 @@ CREATE UNIQUE INDEX "Favorites.phone_unique" ON "Favorites"("phone");
 -- CreateIndex
 CREATE UNIQUE INDEX "Favorites.email_unique" ON "Favorites"("email");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Diary.title_unique" ON "Diary"("title");
+
 -- AddForeignKey
 ALTER TABLE "Contacts" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Favorites" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Diary" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Messages" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
