fe910674aa349cf6b2159f3caf76af889d2386fc
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-14 22:28:33 +0800

    feat: create add favorite functionality

--- src/components/ContactsMobileView.jsx
+++ src/components/ContactsMobileView.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 import CreateNew from './Dialogs/MobileContacts/CreateNew'
 import UpdateContact from './Dialogs/MobileContacts/UpdateContact'
@@ -25,15 +27,15 @@ export default function ContactsMobileView({ online_user, contacts }) {
       </div>
       <Scrollbar>
         <div className="grid grid-rows md:grid-cols-3 grid-flow-row gap-x-4 gap-y-16 w-full max-w-full h-full overflow-y-auto pt-16 pb-5 px-5">
-          {contacts.map(({ id, profile, name, phone, email, address, facebook, twitter, instagram, tiktok, youtube }, i) => (
+          {contacts.map(({ id, profile, name, phone, email, address, facebook, twitter, instagram, tiktok, youtube, favorite }, i) => (
             <div className="flex flex-col w-full max-w-full md:max-w-md h-full rounded-3xl px-5 py-5 space-y-5 bg-modern-dim" key={i}>
               <div className="flex flex-row justify-between items-center w-full">
                 <div className="flex flex-row justify-end">
-                  <button className="text-red-600 transition ease-in-out duration-300 hover:scale-95 focus:outline-none">
-                    <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                    </svg>
-                  </button>
+                  <AddFavorite
+                    online_user={online_user}
+                    id={id}
+                    favorite={favorite}
+                  />
                 </div>
                 <div className="flex justify-center -mt-16 ml-5">
                   <img className="w-24 h-24 object-cover rounded-full" src={profile} />
@@ -102,5 +104,67 @@ export default function ContactsMobileView({ online_user, contacts }) {
         contacts={contacts}
       />
     </div>
+  )
+}
+
+function AddFavorite({ online_user, id, favorite }) {
+
+  const router = useRouter()
+
+  const [isFav, setIsFav] = useState(favorite)
+
+  async function addFavorite() {
+    const userId = online_user.id
+    const contactId = id
+    const favorite = true
+
+    await fetch('/api/favorite/add_favorite', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        userId,
+        contactId,
+        favorite
+      })
+    })
+    setIsFav(true)
+    router.replace(router.asPath)
+  }
+
+  async function removeFavorite() {
+    const userId = online_user.id
+    const contactId = id
+    const favorite = false
+
+    await fetch('/api/favorite/remove_favorite', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        userId,
+        contactId,
+        favorite
+      })
+    })
+    setIsFav(false)
+    router.replace(router.asPath)
+  }
+  
+  return (
+    <>
+      <button onClick={addFavorite} className={`${isFav === false ? 'block' : 'hidden'} text-red-600 transition ease-in-out duration-300 hover:scale-95 focus:outline-none`}>
+        <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+        </svg>
+      </button>
+      <button onClick={removeFavorite} className={`${isFav === true ? 'block' : 'hidden'} text-red-600 transition ease-in-out duration-300 hover:scale-95 focus:outline-none`}>
+        <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd"></path>
+        </svg>
+      </button>
+    </>
   )
 }
---
+++ src/pages/api/favorite/remove_favorite.js
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+  if (req.method === 'PUT') {
+    const remove_favorite = await prisma.contacts.updateMany({
+      where: {
+        id: req.body.contactId,
+        userId: req.body.userId
+      },
+      data: {
+        favorite: req.body.favorite,
+      }
+    })
+    res.json(remove_favorite)
+  } else {
+    res.send("PUT METHOD ONLY!")
+  }
+}
