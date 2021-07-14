0bb767a2c5fc832ad5b22bfa92b8d4cec9dab640
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-14 13:04:06 +0800

    feat: initialize authentication route guards

--- src/pages/diary.jsx
+++ src/pages/diary.jsx
@@ -2,17 +2,21 @@ import Head from 'next/head'
 import Layout from '~/layouts/default'
 import Dashboard from '~/components/Dashboard'
 import DiaryDisplay from '~/components/DiaryDisplay'
+import withSession from '~/lib/Session'
+import { PrismaClient } from '@prisma/client'
 
-export default function Diary() {
+const prisma = new PrismaClient()
+
+export default function Diary({ online_user }) {
   return (
     <>
       <Head>
         <title>Cozy | Diary</title>
       </Head>
-      <Layout>
+      <Layout online_user={online_user}>
         <div className="font-poppins flex flex-col md:flex-row w-full h-full pb-[60px] md:pb-0 rounded-none md:rounded-l-2xl bg-modern-black text-modern-white">
           <div className="hidden md:block w-full max-w-sm">
-            <Dashboard />
+            <Dashboard online_user={online_user} />
           </div>
           <DiaryDisplay />
         </div>
@@ -20,3 +24,30 @@ export default function Diary() {
     </>
   )
 }
+
+export const getServerSideProps = withSession(async function ({ req }) {
+  //check the user session
+  const user = req.session.get('user')
+
+  if(!user) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false
+      }
+    }
+  }
+
+  //find the logged in user from the database
+  const online_user = await prisma.user.findFirst({
+    where: {
+      username: user.username
+    }
+  })
+
+  return {
+    props: {
+      online_user
+    }
+  }
+})
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,19 +2,50 @@ import Head from 'next/head'
 import Layout from '~/layouts/default'
 import Dashboard from '~/components/Dashboard'
 import ContactDisplay from '~/components/ContactDisplay'
+import withSession from '~/lib/Session'
+import { PrismaClient } from '@prisma/client'
 
-export default function Home() {
+const prisma = new PrismaClient()
+
+export default function Home({ online_user }) {
   return (
     <>
       <Head>
         <title>Cozy</title>
       </Head>
-      <Layout>
+      <Layout online_user={online_user}>
         <div className="font-poppins flex flex-col md:flex-row w-full h-full rounded-none md:rounded-l-2xl bg-modern-black text-modern-white">
-          <Dashboard />
+          <Dashboard online_user={online_user} />
           <ContactDisplay />
         </div>
       </Layout>
     </>
   )
 }
+
+export const getServerSideProps = withSession(async function ({ req }) {
+  //check the user session
+  const user = req.session.get('user')
+
+  if(!user) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false
+      }
+    }
+  }
+
+  //find the logged in user from the database
+  const online_user = await prisma.user.findFirst({
+    where: {
+      username: user.username
+    }
+  })
+
+  return {
+    props: {
+      online_user
+    }
+  }
+})
