db476c579915a9d1addb539fc02bb58f4f4db5c1
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-14 16:09:12 +0800

    feat: add update contact functionality

---
+++ src/pages/api/contacts/update.js
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+  if (req.method === 'PUT') {
+    const update = await prisma.contacts.updateMany({
+      where: {
+        id: req.body.contactId,
+        userId: req.body.userId
+      },
+      data: {
+        profile: req.body.profile,
+        name: req.body.name,
+        phone: req.body.phone,
+        email: req.body.email,
+        address: req.body.address,
+        facebook: req.body.facebook,
+        instagram: req.body.instagram,
+        twitter: req.body.twitter,
+        tiktok: req.body.tiktok,
+        youtube: req.body.youtube,
+        userId: req.body.userId
+      }
+    })
+    res.json(update)
+  } else {
+    res.send("PUT METHOD ONLY!")
+  }
+}
