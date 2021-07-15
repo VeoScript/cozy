700cff3574ae2fc796deece0a8ebcab3f6eb0b30
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-15 09:47:22 +0800

    feat: inline password and reenter password inputs

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -113,19 +113,21 @@ export default function SignUp({ all_users }) {
                   <input type="text" name="username" placeholder="Username" className="w-full h-full px-3 py-5 bg-[#1F1F1F] text-honey focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" {...register("username", { required: true })} disabled={isSubmitting} />
                   {errors.username && <span className="flex flex-row justify-end w-full text-[10px] text-honey">Required</span>}
                 </div>
-                <div className="flex items-center w-full px-3 rounded-lg bg-[#1F1F1F]">
-                  <svg className="w-10 h-10 opacity-40" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd"></path>
-                  </svg>
-                  <input type="password" name="password" placeholder="Password" className="w-full h-full px-3 py-5 bg-[#1F1F1F] text-honey focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" {...register("password", { required: true })} disabled={isSubmitting} />
-                  {errors.password && <span className="flex flex-row justify-end w-full text-[10px] text-honey">Required</span>}
-                </div>
-                <div className="flex items-center w-full px-3 rounded-lg bg-[#1F1F1F]">
-                  <svg className="w-10 h-10 opacity-40" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
-                  </svg>
-                  <input type="password" name="repassword" placeholder="Re-enter Password" className="w-full h-full px-3 py-5 bg-[#1F1F1F] text-honey focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" {...register("repassword", { required: true })} disabled={isSubmitting} />
-                  {errors.repassword && <span className="flex flex-row justify-end w-full text-[10px] text-honey">Required</span>}
+                <div className="flex flex-row items-center w-full space-x-2">
+                  <div className="flex items-center w-full px-3 rounded-lg bg-[#1F1F1F]">
+                    <svg className="w-10 h-10 opacity-40" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd"></path>
+                    </svg>
+                    <input type="password" name="password" placeholder="Password" className="w-full h-full px-3 py-5 bg-[#1F1F1F] text-honey focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" {...register("password", { required: true })} disabled={isSubmitting} />
+                    {errors.password && <span className="flex flex-row justify-end w-full text-[10px] text-honey">Required</span>}
+                  </div>
+                  <div className="flex items-center w-full px-3 rounded-lg bg-[#1F1F1F]">
+                    <svg className="w-10 h-10 opacity-40" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
+                    </svg>
+                    <input type="password" name="repassword" placeholder="Re-enter Password" className="w-full h-full px-3 py-5 bg-[#1F1F1F] text-honey focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" {...register("repassword", { required: true })} disabled={isSubmitting} />
+                    {errors.repassword && <span className="flex flex-row justify-end w-full text-[10px] text-honey">Required</span>}
+                  </div>
                 </div>
                 <div className="flex flex-col items-center justify-end w-full space-y-3">
                   <button type="submit" className="flex justify-center w-full max-w-full md:max-w-full px-3 py-4 rounded-lg transition ease-in-out duration-200 transform hover:scale-95 bg-[#1F1F1F] text-modern-white focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" disabled={isSubmitting}>
