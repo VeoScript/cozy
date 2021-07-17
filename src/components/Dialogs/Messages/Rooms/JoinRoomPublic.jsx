e3c62cd2f999231d77dbe63d7618e8fb0b2df0cb
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-17 18:32:43 +0800

    feat: fix join room functionality (#105)

--- src/components/Dialogs/Messages/Rooms/JoinRoomPublic.jsx
+++ src/components/Dialogs/Messages/Rooms/JoinRoomPublic.jsx
@@ -3,12 +3,9 @@ import { useForm } from 'react-hook-form'
 
 export default function JoinRoomPublic({ online_user, id, status, joined_rooms }) {
   
+  // check if the user are already joined in this room
   const checkJoin = joined_rooms.some(joined => joined.indicator === true)
-  const checkJoinedUser = joined_rooms.find(joinedUser => joinedUser.userId === online_user.id)
-
-  const userId = online_user.id
-  const joinedId = checkJoinedUser
-  console.log(userId, joinedId)
+  const checkJoinedUser = joined_rooms.some(joined => joined.userId === online_user.id)
   
   const router = useRouter()
 
@@ -34,12 +31,19 @@ export default function JoinRoomPublic({ online_user, id, status, joined_rooms }
   }
 
   return (
-  <button
-      className={`${status === "Public" ? 'block' : 'hidden'} py-1 w-full max-w-[5rem] rounded-sm font-normal text-[10px] md:text-sm text-modern-black bg-honey transition ease-in-out duration-200 hover:scale-95 focus:outline-none`}
-      type="button"
-      onClick={handleSubmit(onJoin)}
-    >
-      {checkJoin === true ? <span>Joined</span> : <span>Join</span> || userId === joinedId ? <span>Joined</span> : <span>Join</span>}
-    </button>
+    <div className={`${status === "Public" ? 'block' : 'hidden'} w-full max-w-[5rem]`}>
+      <button
+        className={`${checkJoin === true && checkJoinedUser === true ? 'hidden' : 'block'} py-1 w-full rounded-sm font-normal text-[10px] md:text-sm text-modern-black bg-honey transition ease-in-out duration-200 hover:scale-95 focus:outline-none`}
+        type="button"
+        onClick={handleSubmit(onJoin)}
+      >
+        Join
+      </button>
+      <div
+        className={`${checkJoin === true && checkJoinedUser === true ? 'block' : 'hidden'} py-1 w-full text-center rounded-sm font-normal text-[10px] md:text-sm text-modern-white bg-[#333]`}
+      >
+        Joined
+      </div>
+    </div>
   )
 }
