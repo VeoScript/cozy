43c565efbae634e07da89b3d4fec2db3a8c92edb
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-17 12:39:18 +0800

    chore: remove all errors div w-full in text inputs
    Closes #95

--- src/components/Dialogs/MobileContacts/UpdateContact.jsx
+++ src/components/Dialogs/MobileContacts/UpdateContact.jsx
@@ -145,35 +145,35 @@ export default function UpdateContact({ online_user, id, profile, name, phone, e
                             <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd"></path>
                           </svg>
                           <input type="text" name="profile" placeholder="Profile URL" {...register("profile", { required: true })} className="w-full h-full px-3 py-4 bg-[#1F1F1F] text-modern-white focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" disabled={isSubmitting} />
-                          {errors.profile && <span className="flex flex-row justify-end w-full text-[10px] text-honey">Required</span>}
+                          {errors.profile && <span className="flex flex-row justify-end text-[10px] text-honey">Required</span>}
                         </div>
                         <div className="flex items-center w-full px-3 rounded-lg bg-[#1F1F1F]">
                           <svg className="w-8 h-8 opacity-40" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" d="M10 2a1 1 0 00-1 1v1a1 1 0 002 0V3a1 1 0 00-1-1zM4 4h3a3 3 0 006 0h3a2 2 0 012 2v9a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2zm2.5 7a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm2.45 4a2.5 2.5 0 10-4.9 0h4.9zM12 9a1 1 0 100 2h3a1 1 0 100-2h-3zm-1 4a1 1 0 011-1h2a1 1 0 110 2h-2a1 1 0 01-1-1z" clipRule="evenodd"></path>
                           </svg>
                           <input type="text" name="name" placeholder="Name" {...register("name", { required: true })} className="w-full h-full px-3 py-4 bg-[#1F1F1F] text-modern-white focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" disabled={isSubmitting} />
-                          {errors.name && <span className="flex flex-row justify-end w-full text-[10px] text-honey">Required</span>}
+                          {errors.name && <span className="flex flex-row justify-end text-[10px] text-honey">Required</span>}
                         </div>
                         <div className="flex items-center w-full px-3 rounded-lg bg-[#1F1F1F]">
                           <svg className="w-8 h-8 opacity-40" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
                           </svg>
                           <input type="text" name="phone" placeholder="Phone" {...register("phone", { required: true })} className="w-full h-full px-3 py-4 bg-[#1F1F1F] text-modern-white focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" disabled={isSubmitting} />
-                          {errors.phone && <span className="flex flex-row justify-end w-full text-[10px] text-honey">Required</span>}
+                          {errors.phone && <span className="flex flex-row justify-end text-[10px] text-honey">Required</span>}
                         </div>
                         <div className="flex items-center w-full px-3 rounded-lg bg-[#1F1F1F]">
                           <svg className="w-8 h-8 opacity-40" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" d="M14.243 5.757a6 6 0 10-.986 9.284 1 1 0 111.087 1.678A8 8 0 1118 10a3 3 0 01-4.8 2.401A4 4 0 1114 10a1 1 0 102 0c0-1.537-.586-3.07-1.757-4.243zM12 10a2 2 0 10-4 0 2 2 0 004 0z" clipRule="evenodd"></path>
                           </svg>
                           <input type="email" name="email" placeholder="Email" {...register("email", { required: true })} className="w-full h-full px-3 py-4 bg-[#1F1F1F] text-modern-white focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" disabled={isSubmitting} />
-                          {errors.email && <span className="flex flex-row justify-end w-full text-[10px] text-honey">Required</span>}
+                          {errors.email && <span className="flex flex-row justify-end text-[10px] text-honey">Required</span>}
                         </div>
                         <div className="flex items-center w-full px-3 rounded-lg bg-[#1F1F1F]">
                           <svg className="w-8 h-8 opacity-40" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
                           </svg>
                           <input type="text" name="address" placeholder="Address" {...register("address", { required: true })} className="w-full h-full px-3 py-4 bg-[#1F1F1F] text-modern-white focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" disabled={isSubmitting} />
-                          {errors.address && <span className="flex flex-row justify-end w-full text-[10px] text-honey">Required</span>}
+                          {errors.address && <span className="flex flex-row justify-end text-[10px] text-honey">Required</span>}
                         </div>
                       </div>
                       <div className="flex flex-col w-full space-y-3">
