d8523922cfde5e79059c0217ab45a6177f5bf198
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-19 17:07:36 +0800

    feat: create display participants component

--- src/components/MessagesDisplay.jsx
+++ src/components/MessagesDisplay.jsx
@@ -3,6 +3,7 @@ import Scrollbar from 'react-smooth-scrollbar'
 import CreateRoom from './Dialogs/Messages/Rooms/CreateRoom'
 import Discover from './Dialogs/Messages/Rooms/Discover'
 import MyRooms from './Dialogs/Messages/Rooms/MyRooms'
+import Participants from './Dialogs/Messages/Rooms/Participants'
 
 export default function MessagesDisplay({ online_user, rooms, joinedRoom, user_joined_rooms, first_user_joined_rooms, setJoinedRoom }) {
 
@@ -66,10 +67,14 @@ export default function MessagesDisplay({ online_user, rooms, joinedRoom, user_j
                           rooms={rooms}
                         />
                         <hr className="w-full border-t border-modern-dim" />
-                        <button type="button" className="flex items-center w-full text-xs text-gray-400 px-3 py-3 transition ease-in-out duration-300 hover:text-honey space-x-2 focus:outline-none">
-                          <ParticipantsIcon />
-                          <span>Participants</span>
-                        </button>
+                        <Participants
+                          online_user={online_user}
+                          rooms={rooms}
+                          user_joined_rooms={user_joined_rooms}
+                          first_user_joined_rooms={first_user_joined_rooms}
+                          setJoinedRoom={setJoinedRoom}
+                          joinedRoom={joinedRoom}
+                        />
                       </div>
                     </div>
                   </div>
@@ -185,13 +190,5 @@ export default function MessagesDisplay({ online_user, rooms, joinedRoom, user_j
         </Scrollbar>
       </div>
     </div>
-  )
-}
-
-function ParticipantsIcon() {
-  return (
-    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-      <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
-    </svg>
   )
 }
