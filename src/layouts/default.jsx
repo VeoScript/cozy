e092f0406c2d67bf6f0f12e91689e2da775c7a58
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-25 13:32:33 +0800

    fix: change max width in default layout
    Closes #150

--- src/layouts/default.jsx
+++ src/layouts/default.jsx
@@ -3,7 +3,7 @@ import SideBar from '~/components/SideBar'
 export default function Layout({ children, online_user }) {
   return (
     <div className="flex flex-col items-center justify-center w-full max-w-full h-screen overflow-hidden bg-modern-dim">
-      <div className="flex flex-row justify-center w-full max-w-screen-2xl h-full bg-honey">
+      <div className="flex flex-row justify-center w-full max-w-[2400px] h-full bg-honey">
         <SideBar online_user={online_user} />
         { children }
       </div>
