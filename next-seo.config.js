d4cde84f84b1d9448cfdf5be814deb9a7546095d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-21 09:41:10 +0800

    feat: add nextjs search engine optimization (#207)

---
+++ next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://www.veocozy.ml/',
+    site_name: 'VeoCozy',
+  },
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+}
