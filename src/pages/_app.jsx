c7e955b4b6c55031e333f36f8c976ccd6912d1f7
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-27 18:20:16 +0800

    Revert "chore: update search engine optimization (#210)"
    This reverts commit e5cc0a5113c2fa6b0c0c99e54d99a1907c7e96ca.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,13 @@
 import React  from 'react'
 import '~/styles/tailwind.css'
 import NextNProgress from '~/lib/NextProgressbar'
+import { DefaultSeo } from 'next-seo'
+import SEO from 'next-seo.config'
 
 function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
+      <DefaultSeo {...SEO} />
       <NextNProgress />
       <Component {...pageProps} />
     </React.Fragment>
