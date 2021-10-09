3653cdee5ddaee9f55f683443dc9d25554479570
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-10-09 17:01:06 +0800

    style: change private svg color (#211)

--- src/components/Dialogs/Messages/Rooms/Discover.jsx
+++ src/components/Dialogs/Messages/Rooms/Discover.jsx
@@ -139,7 +139,7 @@ export default function Discover({ online_user, rooms }) {
                               <div className="flex flex-col items-start">
                                 <div className="flex flex-row items-center space-x-1">
                                   <span className="font-normal text-sm md:text-base">{ room[2] }</span>
-                                  <svg className={`${room[3] === 'Private' ? 'block' : 'hidden'} w-3 h-3 text-[#58F547]`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                  <svg className={`${room[3] === 'Private' ? 'block' : 'hidden'} w-3 h-3 text-honey`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                     <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd"></path>
                                   </svg>
                                   <svg className={`${room[3] === 'Public' ? 'block' : 'hidden'} w-3 h-3 text-gray-400`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
