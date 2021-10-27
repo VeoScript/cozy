139eedfad159198150594664d2d70e4a3ebd6c13
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-10-27 08:17:52 +0800

    style: change text-color in update name text-input

--- src/components/Dialogs/Profile/EditProfile.jsx
+++ src/components/Dialogs/Profile/EditProfile.jsx
@@ -136,7 +136,7 @@ export default function EditProfile({ online_user }) {
                       <svg className="w-10 h-10 opacity-40" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" d="M10 2a1 1 0 00-1 1v1a1 1 0 002 0V3a1 1 0 00-1-1zM4 4h3a3 3 0 006 0h3a2 2 0 012 2v9a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2zm2.5 7a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm2.45 4a2.5 2.5 0 10-4.9 0h4.9zM12 9a1 1 0 100 2h3a1 1 0 100-2h-3zm-1 4a1 1 0 011-1h2a1 1 0 110 2h-2a1 1 0 01-1-1z" clipRule="evenodd"></path>
                       </svg>
-                      <input type="text" name="name" placeholder="Name" className="w-full h-full px-3 py-5 bg-[#1F1F1F] text-honey focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" {...register("name", { required: true })} disabled={isSubmitting} />
+                      <input type="text" name="name" placeholder="Name" className="w-full h-full px-3 py-5 bg-[#1F1F1F] text-modern-white focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" {...register("name", { required: true })} disabled={isSubmitting} />
                       {errors.name && <span className="flex flex-row justify-end text-[10px] text-honey">Required</span>}
                     </div>
                     <div className="flex items-center w-full px-3 rounded-lg bg-[#1F1F1F]">
